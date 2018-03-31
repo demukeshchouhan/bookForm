389365056d119bccab5afdf206eed44dbe1928e7
demukeshchouhan <chouhan.mukesh17@gmail.com>
2018-03-31 16:40:32 +0530

    change 2

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,11 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Home />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Route path="/" component={Home} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
